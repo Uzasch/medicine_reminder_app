79d8f747c9d0ff50fb4838c36569d773c82d5b9c
Uzasch <uzair2021.cheulkar@gmail.com>
2023-01-29 01:27:50 +0530

    All icons added (Medicine Types)

--- lib/pages/new_entry/new_entry_page.dart
+++ lib/pages/new_entry/new_entry_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:mra/constants.dart';
 import 'package:sizer/sizer.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class NewEntryPage extends StatelessWidget {
+import '../../models/medicine_type.dart';
+
+class NewEntryPage extends StatefulWidget {
   const NewEntryPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewEntryPage> createState() => _NewEntryPageState();
+}
+
+class _NewEntryPageState extends State<NewEntryPage> {
+  late TextEditingController nameController;
+  late TextEditingController dosageController;
+
+  late GlobalKey<ScaffoldState> _scaffoldKey;
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+    dosageController.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    nameController = TextEditingController();
+    dosageController = TextEditingController();
+
+    _scaffoldKey = GlobalKey<ScaffoldState>();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Add New'),
       ),
@@ -22,6 +53,7 @@ class NewEntryPage extends StatelessWidget {
             ),
             TextFormField(
               maxLength: 15,
+              controller: dosageController,
               textCapitalization: TextCapitalization.words,
               decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
@@ -44,9 +76,124 @@ class NewEntryPage extends StatelessWidget {
               style: Theme.of(context).textTheme.subtitle2!.copyWith(
                     color: kOtherColor,
                   ),
-            )
+            ),
+            SizedBox(
+              height: 2.5.h,
+            ),
+            const PanelTitle(title: 'Medicine Type', isRequired: false),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 1.h,
+              ),
+              child: StreamBuilder(
+                builder: (context, snapshot) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MedicineTypeColumn(
+                          medicineType: MedicineType.bottle,
+                          name: 'Bottle',
+                          iconValue: 'assets/icons/bottle.svg',
+                          isSelected: snapshot.data == MedicineType.bottle
+                              ? true
+                              : false),
+                      MedicineTypeColumn(
+                          medicineType: MedicineType.pill,
+                          name: 'Pill',
+                          iconValue: 'assets/icons/pill.svg',
+                          isSelected: snapshot.data == MedicineType.pill
+                              ? true
+                              : false),
+                      MedicineTypeColumn(
+                          medicineType: MedicineType.syringe,
+                          name: 'Syringe',
+                          iconValue: 'assets/icons/syringe.svg',
+                          isSelected: snapshot.data == MedicineType.syringe
+                              ? true
+                              : false),
+                      MedicineTypeColumn(
+                          medicineType: MedicineType.tablet,
+                          name: 'tablet',
+                          iconValue: 'assets/icons/tablet.svg',
+                          isSelected: snapshot.data == MedicineType.tablet
+                              ? true
+                              : false),
+                    ],
+                  );
+                },
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MedicineTypeColumn extends StatelessWidget {
+  const MedicineTypeColumn(
+      {Key? key,
+      required this.medicineType,
+      required this.name,
+      required this.iconValue,
+      required this.isSelected})
+      : super(key: key);
+  final MedicineType medicineType;
+  final String name;
+  final String iconValue;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        //select medicine Type
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 20.w,
+            height: 10.h,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(3.h),
+              color: isSelected ? kOtherColor : Colors.white,
+            ),
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 1.h,
+                  bottom: 0.h,
+                ),
+                child: SvgPicture.asset(
+                  iconValue,
+                  height: 10.h,
+                  color: isSelected ? Colors.white : kOtherColor,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 1.h),
+            child: Container(
+              width: 20.w,
+              height: 5.h,
+              decoration: BoxDecoration(
+                color: isSelected ? kOtherColor : Colors.transparent,
+                borderRadius: BorderRadius.circular(3.h),
+              ),
+              child: Center(
+                child: Text(
+                  name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle2!
+                      .copyWith(color: isSelected ? Colors.white : kOtherColor),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
